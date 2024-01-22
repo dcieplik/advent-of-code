e4f6171f6add9ccffbd42345c0cc3a4a73774db0
Dennis Cieplik <cieplik@optimal-systems.de>
2024-01-22 21:53:00 +0100

    added new duplicate

--- 2022/03/tokenizer/string_test.go
+++ 2022/03/tokenizer/string_test.go
@@ -1,6 +1,7 @@
 package tokenizer_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/dcieplik/advent-of-code/2022/03/tokenizer"
@@ -21,6 +22,59 @@ func TestHalf(t *testing.T) {
 			gotS1, gotS2 := tokenizer.Half(test.s)
 			if gotS1 != test.r1 && gotS2 != test.r2 {
 				t.Fatalf("got '%s' and '%s' but got '%s' and '%s'", gotS1, gotS2, test.r1, test.r2)
+			}
+		})
+	}
+}
+
+// FindDuplicateChars returns a map of duplicate chars.
+func FindDuplicateChars2(ss []string) string {
+
+	find := func(s string) map[string]struct{} {
+		var duplicate = make(map[string]struct{})
+		for _, char := range s {
+
+			duplicate[string(char)] = struct{}{}
+		}
+		return duplicate
+	}
+	var count = make(map[string]int)
+
+	for _, s := range ss {
+		duplicate := find(s)
+		for c := range duplicate {
+			count[string(c)]++
+		}
+	}
+
+	var sb strings.Builder
+	length := len(ss)
+	for key, value := range count {
+		if length == value {
+			sb.WriteString(string(key))
+		}
+	}
+	return sb.String()
+}
+
+func TestFindDuplicateChars2(t *testing.T) {
+
+	tests := map[string]struct {
+		want string
+		s    []string
+	}{
+		"'b' as a duplicate": {s: []string{"aab", "bbb"}, want: "b"},
+		"no duplicate":       {s: []string{"aab", "ccc"}, want: ""},
+		"'ab' as duplicate":  {s: []string{"aab", "aab"}, want: "ab"},
+	}
+	t.Parallel()
+
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			got := FindDuplicateChars2(tc.s)
+			if got != tc.want {
+				t.Fatalf("want '%s' but got '%s'", tc.want, got)
 			}
 		})
 	}
